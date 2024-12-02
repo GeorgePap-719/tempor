8fb6a96bb4e4bd35a20c6456a4d90d61340d704f
George Papadopoulos <george.719pap@gmail.com>
2024-12-02 20:30:37 +0200

    fin

--- src/main/kotlin/Tempor.kt
+++ src/main/kotlin/Tempor.kt
@@ -1,12 +1,22 @@
 package tempor
 
-import kotlin.reflect.KClass
-import kotlin.reflect.safeCast
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.ChannelResult
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.getOrElse
+import kotlinx.coroutines.channels.produce
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.conflate
+import kotlinx.coroutines.flow.consumeAsFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
@@ -19,60 +29,71 @@ class Tempor<T>(
   private val dispatcher: CoroutineDispatcher = Dispatchers.Default.limitedParallelism(1, "temporDispatcher")
 ) {
   private val store = Channel<T>(100_000)
-  private val temporJob: Job
+  private val monitor = Channel<Unit>(0, BufferOverflow.DROP_LATEST)
+
+  private val monitorJob: Job
 
   init {
-    temporJob = initTemporJob()
+    monitorJob = initMonitor()
   }
 
-  // Keep it simple.
-  private fun initTemporJob(): Job = CoroutineScope(dispatcher).launch {
-    while (true) {
-      val slot = store.receive()
-      val consumers = handlers[slot.type] ?: error("there is no registered handler for:${slot::class}")
-      for (consumer in consumers) {
-        val cast = slot.type.safeCast(slot.value)!!
-//        val cast = slot.cast()
-        consumer.handle(cast)
+  private fun initMonitor(): Job = CoroutineScope(dispatcher).launch {
+    //monitor.consumeAsFlow().conflate().flowOn(dispatcher)
+    monitor.consumeAsFlow().collect {
+      println("-- COLLECTING -- ")
+      while (true) {
+        val element = store.tryReceive().getOrElse { return@collect }
+        for (handler in handlers) {
+          launch { handler.handle(element) }
+        }
       }
     }
   }
 
-  fun start() {
-    runBlocking {
-      temporJob.start()
-      temporJob.join()
-    }
+  fun cancelAndJoin(duration: Duration) = runBlocking {
+    delay(duration)
+    // Let's add some clean up operations for easier testing.
+//    store.close()
+    monitorJob.cancelAndJoin()
   }
 
-  fun <T : Any> tryAdd(value: T, type: KClass<T>) {
-    store.trySend(value)
+  fun tryAdd(value: T): ChannelResult<Unit> {
+    val result = store.trySend(value)
+    monitor.trySend(Unit) // ignore failures
+    return result
   }
-
-  private class EventSlot<T : Any>(val value: T, val type: KClass<T>)
 }
-
-
-//@OptIn(ExperimentalCoroutinesApi::class)
-//fun CoroutineScope.fixedPeriodTicker(
-//  delayMillis: Long,
-//): ReceiveChannel<Unit> {
-//  return produce(capacity = 0) {
-//    // We don't really need initial delay.
-//    // delay(delayMillis)
-//    while (true) {
-//      channel.send(Unit)
-//      delay(delayMillis)
-//    }
-//  }
-//}
 
 fun interface EventHandler<T> {
   fun handle(value: T)
 }
 
+val printHandler = EventHandler<String> {
+  println(it)
+}
+val mapHandler = EventHandler<String> {
+  println((it + it)) // result will be ignored anyway
+}
+
+val handlers = listOf(printHandler, mapHandler)
+
 fun main() {
   val engine = Tempor(handlers = handlers)
-  engine.tryAdd("Hello world")
-  engine.start()
+  val result = engine.tryAdd("Hello world ")
+  println(result.isSuccess)
+  engine.cancelAndJoin(2.seconds)
+}
+
+@OptIn(ExperimentalCoroutinesApi::class)
+fun CoroutineScope.fixedPeriodTicker(
+  delayMillis: Long,
+): ReceiveChannel<Unit> {
+  return produce(capacity = 0) {
+    // We don't really need initial delay.
+    // delay(delayMillis)
+    while (true) {
+      channel.send(Unit)
+      delay(delayMillis)
+    }
+  }
 }
